bdbe0f504060db71975906dc9de20f947d87aaf2
Andrew Lever <andylever28@gmail.com>
2017-01-04 19:16:18 -0500

    font fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
+
